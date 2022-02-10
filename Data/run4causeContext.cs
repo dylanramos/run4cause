52e3414f62d39f1afbb8374542dd4a7a3aced3eb
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-02-10 16:32:23 +0100

    Add database files

---
+++ Data/run4causeContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using run4cause.Models;
+
+namespace run4cause.Data
+{
+    public class run4causeContext : DbContext
+    {
+        public run4causeContext (DbContextOptions<run4causeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<run4cause.Models.Participant> Participant { get; set; }
+    }
+}
---
+++ Migrations/20220210130918_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace run4cause.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participant");
+        }
+    }
+}
