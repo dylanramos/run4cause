1d01bbae59746b1e16d7cdf1703a54aa79104385
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-02-10 09:33:30 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace run4cause.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
